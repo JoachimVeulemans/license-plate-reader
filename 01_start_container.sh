b3dc67da82daecab55c79ae6be99d3c2a748131c
Joachim Veulemans <joachim.veulemans@outlook.be>
2020-01-13 01:23:35 +0100

    Introduced arguments

--- 01_start_container.sh
+++ 01_start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it joachimveulemans/license-plate-reader:latest python3 /home/license/LicensePlateReader.py /home/license/
+docker run -it joachimveulemans/license-plate-reader:latest python3 /home/license/LicensePlateReader.py /home/license/ 192.168.1.20
--- app/LicensePlateReader.py
+++ app/LicensePlateReader.py
@@ -90,11 +90,17 @@ class LicensePlateReader:
 
 if __name__ == '__main__':
 	base_dir = ""
-	if len(sys.argv) == 2:
+	camera_ip = "192.168.1.20"
+
+	if (len(sys.argv) != 1 and len(sys.argv) != 3):
+		print("Wrong number of arguments given. First argument is base directory, second argument is ip address of camera.")
+		sys.exit(1)
+
+	if len(sys.argv) == 3:
 		base_dir = sys.argv[1]
+		camera_ip = sys.argv[2]
 
 	filename = base_dir + "images/camera_screen.jpg"
-	camera_ip = "192.168.1.20"
 
 	# ######################################
 	# #### Uncomment below for testing #####
