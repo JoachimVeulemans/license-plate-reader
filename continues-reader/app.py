5790b7b3f39ebdef1432bc0b41e772a087483e8b
Joachim Veulemans <joachim.veulemans@outlook.be>
2020-02-04 14:39:35 +0100

    Edited continues-reader

--- continues-reader/app.py
+++ continues-reader/app.py
@@ -1,16 +1,13 @@
-import numpy as np
 import cv2
-import imutils
-import pytesseract
-import time
 import imageio
-import re
 import sys
-
+import requests
+import time
 
 if __name__ == '__main__':
 	base_dir = ""
 	camera_ip = "192.168.1.20"
+	backend_url = "127.0.0.1:4000/upload"
 
 	if (len(sys.argv) != 1 and len(sys.argv) != 3):
 		print("Wrong number of arguments given. First argument is base directory, second argument is ip address of camera.")
@@ -22,21 +19,15 @@ if __name__ == '__main__':
 
 	filename = base_dir + "images/camera_screen.jpg"
 
-	# ######################################
-	# #### Uncomment below for testing #####
-	# ######################################
-	"""
-	filename = base_dir + "images/image.jpg"
-	print(LicensePlateReader.get_license_plate_text(filename))
-	"""
-
 	cap = cv2.VideoCapture("rtsp://" + camera_ip + ":554/ucast/11")
 
 	while cap.isOpened():
 		try:
 			ret, frame = cap.read()
 			imageio.imsave(filename, frame)
-			print(LicensePlateReader.get_license_plate_text(filename))
+			r = requests.post(url = backend_url, params = {'file': frame})
+			data = r.json() 
+			print(data)
 		except ValueError:
-			print("No license found")
+			print("Camera feed error")
 		time.sleep(1)
